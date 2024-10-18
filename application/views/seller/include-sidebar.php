a00e3b1b39d17f5f2d2cb177028108d7d5c31185
haywill20 <mirandacaceresm6@gmail.com>
2024-10-17 22:12:37 -0600

    feat: Se agregaron las claves para palabras en español de la seccion include-sidebar de seller

--- application/views/seller/include-sidebar.php
+++ application/views/seller/include-sidebar.php
@@ -19,7 +19,7 @@
                     <a href="<?= base_url('seller/home') ?>" class="nav-link">
                         <i class="nav-icon fas fa-home text-primary"></i>
                         <p>
-                            Home
+                            <?= !empty($this->lang->line('menu_home_seller')) ? $this->lang->line('menu_home_seller') : 'Inicio' ?>
                         </p>
                     </a>
                 </li>
@@ -27,7 +27,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-shopping-cart text-warning"></i>
                         <p>
-                            Orders
+                            <?= !empty($this->lang->line('menu_orders_seller')) ? $this->lang->line('menu_orders_seller') : 'Pedidos' ?>
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -35,13 +35,16 @@
                         <li class="nav-item">
                             <a href="<?= base_url('seller/orders/') ?>" class="nav-link">
                                 <i class="fa fa-shopping-cart nav-icon"></i>
-                                <p>Orders</p>
+                                <p><?= !empty($this->lang->line('menu_orders_seller')) ? $this->lang->line('menu_orders_seller') : 'Pedidos' ?>
+                                </p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="<?= base_url('seller/orders/order-tracking') ?>" class="nav-link">
                                 <i class="fa fa-map-marker-alt nav-icon"></i>
-                                <p>Order Tracking</p>
+                                <p>
+                                    <?= !empty($this->lang->line('menu_orders_tracking_seller')) ? $this->lang->line('menu_orders_tracking_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
                     </ul>
@@ -50,7 +53,7 @@
                     <a href="<?= base_url('seller/category/') ?>" class="nav-link">
                         <i class="nav-icon fas fa-bullseye text-success"></i>
                         <p>
-                            Categories
+                            <?= !empty($this->lang->line('menu_categories_seller')) ? $this->lang->line('menu_categories_seller') : 'Null' ?>
                         </p>
                     </a>
                 </li>
@@ -59,7 +62,7 @@
                     <a href="<?= base_url('seller/point_of_sale/') ?>" class="nav-link">
                         <i class="nav-icon fas fa-calculator"></i>
                         <p>
-                            Point Of Sale
+                            <?= !empty($this->lang->line('menu_point_of_sale_seller')) ? $this->lang->line('menu_point_of_sale_seller') : 'Null' ?>
                         </p>
                     </a>
                 </li>
@@ -68,7 +71,7 @@
                     <a href="<?= base_url('seller/manage_stock') ?>" class="nav-link">
                         <i class="nav-icon fa fa-cube text-success"></i>
                         <p>
-                            Manage Stock
+                            <?= !empty($this->lang->line('menu_manage_stock_seller')) ? $this->lang->line('menu_manage_stock_seller') : 'Null' ?>
                         </p>
                     </a>
                 </li>
@@ -77,7 +80,7 @@
                     <a href="<?= base_url('seller/chat') ?>" class="nav-link">
                         <i class="nav-icon 	far fa-comments text-warning"></i>
                         <p>
-                            Chat
+                            <?= !empty($this->lang->line('menu_chat_seller')) ? $this->lang->line('menu_chat_seller') : 'Null' ?>
                         </p>
                     </a>
                 </li>
@@ -87,7 +90,7 @@
                     <a href="#" class="nav-link menu-open">
                         <i class="nav-icon fas fa-cubes text-danger"></i>
                         <p>
-                            Products
+                            <?= !empty($this->lang->line('menu_products_seller')) ? $this->lang->line('menu_products_seller') : 'Null' ?>
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -97,21 +100,24 @@
                         <li class="nav-item">
                             <a href="<?= base_url('seller/attribute_set/') ?>" class="nav-link">
                                 <i class="fa fa-cogs nav-icon"></i>
-                                <p>Attribute Sets</p>
+                                <p><?= !empty($this->lang->line('menu_attribute_sets_seller')) ? $this->lang->line('menu_attribute_sets_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
                         <li class="nav-item">
                             <a href="<?= base_url('seller/attributes/') ?>" class="nav-link">
                                 <i class="fas fa-sliders-h nav-icon"></i>
-                                <p>Attributes</p>
+                                <p><?= !empty($this->lang->line('menu_attributes_seller')) ? $this->lang->line('menu_attributes_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
                         <li class="nav-item">
                             <a href="<?= base_url('seller/attribute_value/') ?>" class="nav-link">
                                 <i class="fas fa-filter nav-icon"></i>
-                                <p>Attribute Values</p>
+                                <p><?= !empty($this->lang->line('menu_attributes_values_seller')) ? $this->lang->line('menu_attributes_values_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
@@ -119,34 +125,39 @@
                         <li class="nav-item">
                             <a href="<?= base_url('seller/taxes/') ?>" class="nav-link">
                                 <i class="fas fa-percentage nav-icon"></i>
-                                <p>Tax</p>
+                                <p><?= !empty($this->lang->line('menu_tax_seller')) ? $this->lang->line('menu_tax_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
                         <li class="nav-item">
                             <a href="<?= base_url('seller/product/create-product') ?>" class="nav-link">
                                 <i class="fas fa-plus-square nav-icon"></i>
-                                <p>Add Products</p>
+                                <p><?= !empty($this->lang->line('menu_add_products_seller')) ? $this->lang->line('menu_add_products_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
                         <li class="nav-item">
                             <a href="<?= base_url('seller/product/bulk-upload') ?>" class="nav-link">
                                 <i class="fas fa-upload nav-icon"></i>
-                                <p>Bulk upload</p>
+                                <p><?= !empty($this->lang->line('menu_bulk_upload_seller')) ? $this->lang->line('menu_bulk_upload_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
                         <li class="nav-item">
                             <a href="<?= base_url('seller/product/') ?>" class="nav-link">
                                 <i class="fas fa-boxes nav-icon"></i>
-                                <p>Manage Products</p>
+                                <p><?= !empty($this->lang->line('menu_manage_products_seller')) ? $this->lang->line('menu_manage_products_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="<?= base_url('seller/product_faqs/') ?>" class="nav-link">
                                 <i class="fas fa-question-circle nav-icon"></i>
-                                <p>Product FAQs</p>
+                                <p><?= !empty($this->lang->line('menu_products_faqs_seller')) ? $this->lang->line('menu_products_faqs_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
                     </ul>
@@ -155,20 +166,22 @@
                     <a href="<?= base_url('seller/media/') ?>" class="nav-link">
                         <i class="nav-icon fas fa-icons text-success"></i>
                         <p>
-                            Media
+                            <?= !empty($this->lang->line('menu_media_seller')) ? $this->lang->line('menu_media_seller') : 'Null' ?>
                         </p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="<?= base_url('seller/transaction/wallet-transactions') ?>" class="nav-link">
                         <i class="fa fa-rupee-sign nav-icon text-warning"></i>
-                        <p>Wallet Transactions</p>
+                        <p><?= !empty($this->lang->line('menu_wallet_transactions_seller')) ? $this->lang->line('menu_wallet_transactions_seller') : 'Null' ?>
+                        </p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="<?= base_url('seller/payment-request/withdrawal-requests') ?>" class="nav-link">
                         <i class="nav-icon fas fa-money-bill-wave text-danger"></i>
-                        <p> Withdrawal Requests</p>
+                        <p><?= !empty($this->lang->line('menu_withdrawal_requests_seller')) ? $this->lang->line('menu_withdrawal_requests_seller') : 'Null' ?>
+                        </p>
                     </a>
                 </li>
 
@@ -176,7 +189,7 @@
                     <a href="<?= base_url('seller/Pickup_location/manage_pickup_locations') ?>" class="nav-link">
                         <i class="nav-icon fas fa-shipping-fast text-success"></i>
                         <p>
-                            Pickup Location
+                            <?= !empty($this->lang->line('menu_pickup_location_seller')) ? $this->lang->line('menu_pickup_location_seller') : 'Null' ?>
                         </p>
                     </a>
                 </li>
@@ -185,7 +198,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-map-marked-alt text-info"></i>
                         <p>
-                            Location
+                            <?= !empty($this->lang->line('menu_location_seller')) ? $this->lang->line('menu_location_seller') : 'Null' ?>
                             <i class="right fas fa-angle-left "></i>
                         </p>
                     </a>
@@ -193,13 +206,15 @@
                         <li class="nav-item">
                             <a href="<?= base_url('seller/area/manage-zipcodes') ?>" class="nav-link">
                                 <i class="fa fa-map-pin nav-icon "></i>
-                                <p>Zipcodes</p>
+                                <p><?= !empty($this->lang->line('menu_zipcodes_seller')) ? $this->lang->line('menu_zipcodes_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="<?= base_url('seller/area/manage-cities') ?>" class="nav-link">
                                 <i class="fa fa-location-arrow nav-icon "></i>
-                                <p>City</p>
+                                <p><?= !empty($this->lang->line('menu_city_seller')) ? $this->lang->line('menu_city_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
 
@@ -207,7 +222,7 @@
                             <a href="<?= base_url('seller/area/manage-areas') ?>" class="nav-link">
                                 <i class="fas fa-street-view nav-icon "></i>
                                 <p>
-                                    Areas
+                                    <?= !empty($this->lang->line('menu_areas_seller')) ? $this->lang->line('menu_areas_seller') : 'Null' ?>
                                 </p>
                             </a>
                         </li>
@@ -215,7 +230,7 @@
                             <a href="<?= base_url('seller/area/manage_countries') ?>" class="nav-link">
                                 <i class="fas fa-solid fa-globe nav-icon "></i>
                                 <p>
-                                    Countries
+                                    <?= !empty($this->lang->line('menu_countries_seller')) ? $this->lang->line('menu_countries_seller') : 'Null' ?>
                                 </p>
                             </a>
                         </li>
@@ -225,7 +240,7 @@
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="fas fa-chart-pie nav-icon text-primary"></i>
-                        <p>Reports
+                        <p><?= !empty($this->lang->line('menu_reports_seller')) ? $this->lang->line('menu_reports_seller') : 'Null' ?>
                             <i class="right fas fa-angle-left "></i>
                         </p>
                     </a>
@@ -233,13 +248,15 @@
                         <li class="nav-item">
                             <a href="<?= base_url('seller/sales-report') ?>" class="nav-link">
                                 <i class="fa fa-chart-line nav-icon "></i>
-                                <p>Sales Report</p>
+                                <p><?= !empty($this->lang->line('menu_sales_report_seller')) ? $this->lang->line('menu_sales_report_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="<?= base_url('seller/sales-inventory') ?>" class="nav-link">
                                 <i class="fa fa-chart-line nav-icon "></i>
-                                <p>Sale Inventory Reports</p>
+                                <p><?= !empty($this->lang->line('menu_sales_inventory_reports_seller')) ? $this->lang->line('menu_sales_inventory_reports_seller') : 'Null' ?>
+                                </p>
                             </a>
                         </li>
                     </ul>
